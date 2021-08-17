a46a93b9f1b685ddb5c2b8be1f304cdf3dc8a395
1161678627 <1161678627@qq.com>
2021-08-18 00:05:17 +0800

    c++如何定义变量以及如何print

--- c-learning/helloworld.cpp
+++ c-learning/helloworld.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+// 常量的定义 #define,在文件头部声明
+#define Day 7
+// 使用const 标记变量为 常量，const定义一般写在函数内
+const int month = 24;
+// month = 10;	修改常量会提示报错
+
+int main1() {
+
+	cout << "一周一共有：" << Day << "天" << endl;
+	// 变量的定义及赋值
 	int a;
 	a = 10;
-	cout << "a = " << a << endl;
+	int b = 10;
+	float sum = a + b;
+	cout << "a + b = " << sum << endl;
 	// cout << "hello world" << endl;
 	system("pause");
 	return 0;
